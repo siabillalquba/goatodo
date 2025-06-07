7e329bb30342fc420e5dc5ddbfe1a4e55e87a29c
Sia Billal Quba <siabillal@gmail.com>
2025-06-08 01:27:26 +0700

    feat: home and about routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import "./index.css";
-import { App } from "./app";
+import { HomeRoute } from "./routes/home";
+import { AboutRoute } from "./routes/about";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<HomeRoute />} />
+      <Route path="/about" element={<AboutRoute />} />
     </Routes>
   </BrowserRouter>,
 );
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,11 @@
+import { App } from "../app";
+
 export function HomeRoute() {
   return (
     <div>
       <h1>Home Route</h1>
+
+      <App />
     </div>
   );
 }
