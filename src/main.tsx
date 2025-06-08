d72e33e777536283b58a401ba7bd5e56137e6b0b
Sia Billal Quba <siabillal@gmail.com>
2025-06-08 12:14:16 +0700

    refactor: app to wrap router config

--- src/main.tsx
+++ src/main.tsx
@@ -6,23 +6,27 @@ import { HomeRoute } from "./routes/home";
 import { AboutRoute } from "./routes/about";
 import { TaskDetailsRoute } from "./routes/task-details";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <nav className="bg-white p-4 text-sm">
-      <ul className="flex gap-4">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-    </nav>
+createRoot(document.getElementById("root")!).render(<App />);
 
-    <Routes>
-      <Route path="/" element={<HomeRoute />} />
-      <Route path="/about" element={<AboutRoute />} />
-      <Route path="/tasks/:taskID" element={<TaskDetailsRoute />} />
-    </Routes>
-  </BrowserRouter>,
-);
+function App() {
+  return (
+    <BrowserRouter>
+      <nav className="bg-white p-4 text-sm">
+        <ul className="flex gap-4">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomeRoute />} />
+        <Route path="/about" element={<AboutRoute />} />
+        <Route path="/tasks/:taskID" element={<TaskDetailsRoute />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
